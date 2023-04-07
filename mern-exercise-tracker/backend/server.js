20c43de4130d8da0de03c782585cc2980d8cbdcd
Gavin Qian <gqia0002@student.monash.edu>
2023-04-07 14:46:13 +1000

    Finished backend for MERN tutorial

---
+++ mern-exercise-tracker/backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+console.log(process.env.ATLAS_URI);
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
