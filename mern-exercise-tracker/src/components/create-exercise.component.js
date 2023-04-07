50ae530da8a607893fec5111e12d1142aa90f920
Gavin Qian <gqia0002@student.monash.edu>
2023-04-07 16:00:53 +1000

    Connected the completed frontend and backend for the MERN tutorial

--- mern-exercise-tracker/src/components/create-exercise.component.js
+++ mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -22,7 +23,14 @@ export default class CreateExercise extends Component {
     }
 
     componentDidMount() {
-        this.setState({ users: ["john doe"], username: "john doe" });
+        axios.get("http://localhost:5000/users").then((response) => {
+            if (response.data.length > 0) {
+                this.setState({
+                    users: response.data.map((user) => user.username),
+                    username: response.data[0].username,
+                });
+            }
+        });
     }
 
     onChangeUsername(e) {
@@ -52,6 +60,10 @@ export default class CreateExercise extends Component {
         };
 
         console.log(exercise);
+
+        axios
+            .post("http://localhost:5000/exercises/add", exercise)
+            .then((res) => console.log(res.data));
 
         window.location = "/";
     }
