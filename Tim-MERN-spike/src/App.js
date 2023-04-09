c3d3bda8a6bea1eb7819460b5dafca1615d956a3
Timothy Tan <tt.ttmain@gmail.com>
2023-04-09 18:24:03 +1000

    Created MERN spike

---
+++ Tim-MERN-spike/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+    return (
+        <Router>
+            <div className="container">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<ExercisesList />} />
+                    <Route path="/edit/:id" element={<EditExercise />} />
+                    <Route path="/create" element={<CreateExercise />} />
+                    <Route path="/user" element={<CreateUser />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
