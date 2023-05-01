fcba9d38180104bda2edb8069f7b034fdaa068e5
Zoe Parkinson <zpar0002@student.monash.edu>
2023-05-01 18:56:05 +1000

    [done] Worked on upload transcript feature. Can now upload transcripts and store in database

--- project/meeting-summariser/src/components/Button/Button.jsx
+++ project/meeting-summariser/src/components/Button/Button.jsx
@@ -1,12 +1,20 @@
 import './Button.css'
 import { AiFillPlusCircle } from 'react-icons/ai';
+import axios from "axios"
 
 const Button = () => {
     const handleChange = (e) => {
         e.preventDefault();
     
         const formData = new FormData();
-        // formData.append("photo", e.target.files[0]);
+        formData.append("transcript", e.target.files[0]);
+
+        axios
+            .post("http://localhost:5000/api/save", formData)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => console.log(err.response.data))
       };
 
 	return (
