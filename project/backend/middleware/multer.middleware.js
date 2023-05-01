0e7c5ecafc7b7dd724f45b2cbbfe140b82dafdb9
Zoe Parkinson <zpar0002@student.monash.edu>
2023-05-01 21:14:52 +1000

    [done] - Removed transcripts saving on local device

--- project/backend/middleware/multer.middleware.js
+++ project/backend/middleware/multer.middleware.js
@@ -3,9 +3,6 @@ const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./uploads/transcripts");
-    },
     filename: function (req, file, cb) {
         cb(null, `${uuidv4()}_${path.extname(file.originalname)}`);
     },
