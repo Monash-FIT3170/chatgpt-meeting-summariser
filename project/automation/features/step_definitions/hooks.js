ca2065d1aad47f1b6c6d318dd6e0b1de8b2664e2
James Harvey <jamesh@gutenmorganfreeman.com>
2023-05-08 18:59:02 +1000

    Example now done just to do a self review before pr

--- project/automation/features/step_definitions/hooks.js
+++ project/automation/features/step_definitions/hooks.js
@@ -1,4 +1,4 @@
-const { Given, When, Then, After } = require('@cucumber/cucumber');
+const { After } = require('@cucumber/cucumber');
 
 
 require("chromedriver");
--- project/automation/features/step_definitions/sample.js
+++ project/automation/features/step_definitions/sample.js
@@ -1,26 +1,28 @@
 const { expect } = require('chai');
-const { Given, When, Then, After } = require('@cucumber/cucumber');
+const { Given, When, Then, After, Before } = require('@cucumber/cucumber');
 
-const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
+const GoogleSearchPage = require('../../pages/googleSearchPage');
 
 require("chromedriver");
 
+let googleSearchPage;
+
+Before(async function () {
+    googleSearchPage = new GoogleSearchPage(this.driver);
+})
 
 
 Given('I am on the Google search page', async function () {
-    await this.driver.get('http://www.google.com');
+    await googleSearchPage.goToUrl("http://www.google.com");
 });
 
 When('I search for {string}', async function (searchTerm) {
-    const element = await this.driver.findElement(By.name('q'));
-    await element.click()
-    await element.clear()
-    await element.sendKeys(searchTerm);
-    await element.submit();
+    await googleSearchPage.enterSearchKey(searchTerm);
+    await googleSearchPage.submitSearch();
 });
 
 Then('the page title should start with {string}', { timeout: 60 * 1000 }, async function (searchTerm) {
-    const title = await this.driver.getTitle();
+    const title = await googleSearchPage.getTitle()
     const isTitleStartWithCheese = title.toLowerCase().lastIndexOf(`${searchTerm}`, 0) === 0;
     expect(isTitleStartWithCheese).to.equal(true);
 });
