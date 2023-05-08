c283af1994158a5be6fe4832f69a58ebc44a54ae
jwig0002 <jwig0002@student.monash.edu>
2023-05-08 18:57:55 +1000

    added openssl

---
+++ project/meeting-summariser/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Navbar } from "../components"
+
+const Home = () => {
+    return (
+        <>
+        <Navbar title={"Dashboard"}></Navbar>
+        <div>
+            Home
+        </div>
+        </>
+        
+    )
+}
+
+export default Home
