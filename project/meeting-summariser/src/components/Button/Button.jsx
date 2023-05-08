ab94178deb437c0e4581dc7d3d54c2841999a03b
elim0050 <elim0050@student.monash.edu>
2023-05-08 19:03:57 +1000

    Assign filename to the tuploaded file

--- project/meeting-summariser/src/components/Button/Button.jsx
+++ project/meeting-summariser/src/components/Button/Button.jsx
@@ -8,23 +8,37 @@ const Button = () => {
         const formData = new FormData();
         formData.append("transcript", e.target.files[0]);
 
+        //check if any file is chosen and set the file name 
+        if (e.target.files.length> 0){
+            var file_node= document.getElementById("file")
+            var name = e.target.files[0].name
+            var div= document.createElement("div");
+            div.className= "file_button"
+            div.innerText = name 
+            file_node.appendChild(div)
+        }
+
+
         axios
             .post("http://localhost:5000/api/save", formData)
             .then((res) => {
                 console.log(res.data);
             })
             .catch((err) => console.log(err.response.data))
-      };
-
+      }
 	return (
-		<div>
-            <label className='button' htmlFor="file_picker">
+
+        <div id="file">
+            <label className='button' htmlFor="file_picker" >
                 Upload Recording
-			<input hidden type="file" name="file_picker" id="file_picker" onChange={(e) => handleChange(e)}/>
-            </label>
-			
-		</div>
+            <input hidden type="file" name="file_picker" id="file_picker" onChange={(e) => handleChange(e)}/>
+            </label>			
+        </div>
+
 	);
 };
+
+
+
 
 export default Button;
