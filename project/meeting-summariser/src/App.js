6f8a68090f869ee48534c81e8e83934c72746354
ttan0053 <ttan0053@student.monash.edu>
2023-05-15 22:36:41 +1000

    Fixed merge conflict

--- project/meeting-summariser/src/App.js
+++ project/meeting-summariser/src/App.js
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home, Summarizer } from './pages'
 import CreateMeetingSummary from './components/createMeetingSummary.component'
-import { Home } from './pages'
->>>>>>>>> Temporary merge branch 2
 import './App.css';
 import LoginCanvas from './LoginModules/Login';
 import CreateAccount from './LoginModules/CreateAccount';
