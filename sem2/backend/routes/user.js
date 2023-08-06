7846e37478c85155bb54b55020d83ddb710fa8a0
James Harvey <jhar0055@student.monash.edu>
2023-08-07 09:25:51 +1000

    finished thing, first test working as an example

--- sem2/backend/routes/user.js
+++ sem2/backend/routes/user.js
@@ -75,5 +75,3 @@ router.post('/login', async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }   
 });
-
-    module.exports = router;
--- sem2/backend/tests/user.test.js
+++ sem2/backend/tests/user.test.js
@@ -1,4 +1,11 @@
 const createUser = require("../routes/user");
+const db = require('../db');
+
+beforeAll(async () => await db.connect())
+
+afterEach(async () => await db.clearDatabase())
+
+afterAll(async () => await db.closeDatabase())
 
 test("Create user sucessfully", async () => {
     let user = await createUser('firstname', 'lastname', 'test@gmail.com', 'password123');
