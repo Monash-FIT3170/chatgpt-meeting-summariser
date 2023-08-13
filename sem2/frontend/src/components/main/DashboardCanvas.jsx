3d9602a7d424aa234dca00d96ca1737cc6505ffe
Gavin Qian <gqia0002@student.monash.edu>
2023-08-13 17:13:47 +1000

    Refactored code to be more modular for different screens.

--- sem2/frontend/src/components/main/DashboardCanvas.jsx
+++ sem2/frontend/src/components/main/DashboardCanvas.jsx
@@ -7,33 +7,49 @@ import { MeetingsScreen } from './MeetingsScreen';
 import { HeaderPill } from '../HeaderPill';
 
 function DashboardCanvas() {
-    const [isUploadActive, setUploadIsActive] = useState(false);
-    const [isDashboardActive, setDashboardIsActive] = useState(true);
-    const [isYourMeetingsActive, setYourMeetingsIsActive] = useState(false);
-    const handleUploadClick = () => {
-        setUploadIsActive(true);
-        setDashboardIsActive(false);
-        setYourMeetingsIsActive(false);
-    };
-    const handleDashboardClick = () => {
-        setUploadIsActive(false);
-        setDashboardIsActive(true);
-        setYourMeetingsIsActive(false);
-    };
-    const handleYourMeetingsClick = () => {
-        setUploadIsActive(false);
-        setDashboardIsActive(false);
-        setYourMeetingsIsActive(true);
-    };
+    // const [isUploadActive, setUploadIsActive] = useState(false);
+    // const [isDashboardActive, setDashboardIsActive] = useState(true);
+    // const [isYourMeetingsActive, setYourMeetingsIsActive] = useState(false);
+    // const handleUploadClick = () => {
+    //     setUploadIsActive(true);
+    //     setDashboardIsActive(false);
+    //     setYourMeetingsIsActive(false);
+    // };
+    // const handleDashboardClick = () => {
+    //     setUploadIsActive(false);
+    //     setDashboardIsActive(true);
+    //     setYourMeetingsIsActive(false);
+    // };
+    // const handleYourMeetingsClick = () => {
+    //     setUploadIsActive(false);
+    //     setDashboardIsActive(false);
+    //     setYourMeetingsIsActive(true);
+    // };
+
+    const [activeScreen, setActiveScreen] = useState("Dashboard");
+
+    const handleUploadClick = () => {setActiveScreen("Upload")};
+    const handleDashboardClick = () => {setActiveScreen("Dashboard")};
+    const handleYourMeetingsClick = () => {setActiveScreen("Meetings")};
+
+    const screenComponents = {
+        "Dashboard": <DashboardScreen onUploadClick={handleUploadClick}/>,
+        "Upload": <UploadScreen/>,
+        "Meetings": <MeetingsScreen/>
+    }
+
     return (
         <>
             <div className={BasicStyles.body}>
                 <div className={BasicStyles.header2}>
-                    <HeaderPill name="Dashboard" onClick={handleDashboardClick} isActive={isDashboardActive} />
-                    <HeaderPill name="Upload" onClick={handleUploadClick} isActive={isUploadActive} />
-                    <HeaderPill name="YourMeetings" onClick={handleYourMeetingsClick} isActive={isYourMeetingsActive} />
+                    <HeaderPill name="Dashboard" onClick={handleDashboardClick} isActive={activeScreen === "Dashboard"} />
+                    <HeaderPill name="Upload" onClick={handleUploadClick} isActive={activeScreen === "Upload"} />
+                    <HeaderPill name="YourMeetings" onClick={handleYourMeetingsClick} isActive={activeScreen === "Meetings"} />
                 </div>
-                <div style={{
+                <div>
+                    {screenComponents[activeScreen]}
+                </div>
+                {/* <div style={{
                     display: isUploadActive ? "block" : "none",
                 }}>
                     <UploadScreen></UploadScreen>
@@ -47,7 +63,7 @@ function DashboardCanvas() {
                     display: isYourMeetingsActive ? "block" : "none"
                 }}>
                     <MeetingsScreen/>
-                </div>
+                </div> */}
             </div>
         </>
     );
