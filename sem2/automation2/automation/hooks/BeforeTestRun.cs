b3a8cbbe5eed367d2dbc7ec971987b12f22ff708
James <jhar0055@student.monash.edu>
2023-08-13 21:19:33 +1000

    wip, c# automation

---
+++ sem2/automation2/automation/hooks/BeforeTestRun.cs
@@ -0,0 +1,21 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Hooks;
+
+[Binding]
+public class BeforeTestRun
+{
+    private ScenarioContext _scenarioContext;
+
+    public BeforeTestRun(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [BeforeTestRun]
+    public void BeforeTest()
+    {
+        //reset the world
+
+    }
+}
