dd19062d47d3550b0b385eac2a7b7aec5f4ad7af
jwig0002 <jwig0002@student.monash.edu>
2023-08-14 09:36:05 +1000

    Upload screen done, working on meeting participant

--- sem2/frontend/src/components/main/UploadScreen.jsx
+++ sem2/frontend/src/components/main/UploadScreen.jsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
+import { HeaderPill } from '../HeaderPill';
 import styles from './Dashboard.module.css';
 
 function UploadScreen() {
-    const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
-    const [isProcessing, setIsProcessing] = useState(false);
+    const [activeScreen, setActiveScreen] = useState("RecordingUpload");
+    const handleRecordingUploadClick = () => { setActiveScreen("RecordingUpload") };
+    const handleMeetingParticipantsClick = () => { setActiveScreen("MeetingParticipants") };
 
+    const screenComponents = {
+        "RecordingUpload": <RecordingUploadScreen />,
+        "MeetingParticipants": <MeetingParticipantsScreen />
+    }
+
+
+    return (
+        <>
+            <div style={{
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "center",
+            }}>
+                <div className={styles.upload_menu_bar}>
+                    <HeaderPill name="Recording Upload" onClick={handleRecordingUploadClick} isActive={activeScreen === "RecordingUpload"}></HeaderPill>
+                    <HeaderPill name="Meeting Participants" onClick={handleMeetingParticipantsClick} isActive={activeScreen === "MeetingParticipants"}></HeaderPill>
+                </div>
+            </div>
+            <div>
+                {screenComponents[activeScreen]}
+            </div>
+        </>
+    )
+}
+
+function RecordingUploadScreen() {
     const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-	    setIsFilePicked(true);
-        document.getElementById("filename").innerText=event.target.files[0].name;
-	};
+        setSelectedFile(event.target.files[0]);
+        setIsFilePicked(true);
+        document.getElementById("filename").innerText = event.target.files[0].name;
+    };
+
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+    const [isProcessing, setIsProcessing] = useState(false);
 
     return (
         <>
             <div className={styles.upload_page}>
-                <div className={styles.upload_menu_bar}>
-                    Menu Bar TBD
-                </div>
                 <div className={styles.upload_heading}>Upload new recording</div>
                 <div className={styles.file_upload_box}>
                     <div className={styles.file_upload_header_1}>
@@ -28,11 +56,11 @@ function UploadScreen() {
                     </div>
                     <div className={styles.file_upload}>
                         <div className={styles.upload_microphone}>
-                            
+
                         </div>
                         <div className={styles.file_name} id="filename">filename.mp4</div>
                         <label for="upload-btn">Browse</label>
-                        <input type="file" id="upload-btn" hidden onChange={changeHandler}/>
+                        <input type="file" id="upload-btn" hidden onChange={changeHandler} />
                     </div>
 
                 </div>
@@ -75,4 +103,64 @@ function UploadScreen() {
     )
 }
 
-export {UploadScreen};
+function MeetingParticipantsScreen(children, styles, className) {
+    return (
+        <>
+            <div className={styles.summary_heading}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="656" height="16" viewBox="0 0 656 16" className={styles.summary_heading_line}>
+                    <path d="M639.993 8.03713C640.014 12.4554 643.612 16.0204 648.03 15.9999C652.448 15.9795 656.013 12.3812 655.993 7.96294C655.972 3.54471 652.374 -0.0203682 647.956 0.000118163C643.538 0.0206045 639.973 3.6189 639.993 8.03713ZM1.00696 12.5L648 9.50002L647.986 6.50005L0.993045 9.50002L1.00696 12.5Z" fill="#E9F9FF" fill-opacity="0.25" />
+                </svg>
+                Edit Meeting Participants
+                <svg xmlns="http://www.w3.org/2000/svg" width="651" height="16" viewBox="0 0 651 16" className={styles.summary_heading_line}>
+                    <path d="M3.86238e-05 7.97508C-0.0137254 12.3933 3.55682 15.9862 7.97508 16C12.3933 16.0137 15.9862 12.4432 16 8.02492C16.0137 3.60667 12.4432 0.0138027 8.02492 3.86238e-05C3.60667 -0.0137254 0.0138027 3.55682 3.86238e-05 7.97508ZM7.99533 9.49999L649.995 11.5L650.005 8.50001L8.00467 6.50001L7.99533 9.49999Z" fill="#E9F9FF" fill-opacity="0.25" />
+                </svg>
+            </div>
+            <div style={{
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "center",
+            }}>
+                <div className={styles.participants_header}>
+                    <div className={styles.participants_header_text}>
+                        #
+                    </div>
+                    <div className={styles.participants_header_text}>
+                        Participant Name
+                    </div>
+                    <div className={styles.participants_header_text}>
+                        Participant Email
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+function MeetingParticipant(number, name, email) {
+    return (
+        <>
+            <div className={styles.participant_text}>
+                {number}
+                {name}
+                {email}
+            </div>
+        </>
+    )
+}
+
+function MeetingParticipants(children) {
+    let participants_list = React.Children.toArray(this.props.children)
+
+    return (
+        <>
+            <div className={styles.participants}>
+            React.Children.map(children, (child, index) => {
+                    <MeetingParticipant number={index} name={child.name} email={child.email} />
+                })
+            </div>
+        </>
+    )
+}
+
+export { UploadScreen };
