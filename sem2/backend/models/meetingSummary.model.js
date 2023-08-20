4b48aaf708ac99e745dee9aa9d53270753b9fbed
tkha0014 <tkha0014@student.monash.edu>
2023-08-20 22:39:05 +1000

    Updating the meetingSummary model with attenddes

--- sem2/backend/models/meetingSummary.model.js
+++ sem2/backend/models/meetingSummary.model.js
@@ -6,6 +6,7 @@ const meetingSummarySchema = new Schema(
     _id: mongoose.Schema.Types.ObjectId,
     transcript: { type: String, required: true },
     summaryPoints: { type: [String], required: true },
+    attendees: { type: [String], required: false },
   },
   {
     timestamps: true,
