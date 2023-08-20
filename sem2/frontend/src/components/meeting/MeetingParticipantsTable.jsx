a653f64b9068724efebdfe10c1755773744ae328
Gavin Qian <gqia0002@student.monash.edu>
2023-08-20 18:45:42 +1000

    [WIP] Finished meeting participants component styling. Just missing the "add participant" button.

--- sem2/frontend/src/components/meeting/MeetingParticipantsTable.jsx
+++ sem2/frontend/src/components/meeting/MeetingParticipantsTable.jsx
@@ -1,22 +1,53 @@
-import React, { useState } from 'react';
-import styles from './meeting.css';
+import React, { useState } from "react";
+import styles from "./meeting.module.css";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
-function MeetingParticipantsTable({meetingId}) {
-    const [participants, setParticipants] = useState([]);
+function MeetingParticipantsTable({ participants }) {
+    if (participants?.length === 0) {
+        return <p>No participants available.</p>;
+    }
 
     return (
         <>
-            <div className={styles.participants_table_header}>
-                <table className={styles.participants_table}>
-                    <tr>
-                        <th>#</th>
-                        <th>Participant Name</th>
-                        <th>Participant Email</th>
-                    </tr>
-                </table>
+            <div
+                style={{
+                    alignItems: "center",
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <div className={styles.meeting_participants_box}>
+                    <div className={styles.meeting_participants_header}>
+                        <div className={styles.t1}>#</div>
+                        <div className={styles.t2}>Participant Name</div>
+                        <div className={styles.t3}>Participant Email</div>
+                        <div className={styles.t4}></div>
+                    </div>
+                    <div className={styles.meeting_participants_table}>
+                        {participants?.map((participant, index) => (
+                            <div className={styles.participant_line}>
+                                <div className={styles.t1}>{index + 1}</div>
+                                <div className={styles.t2}>
+                                    {participant?.name}
+                                </div>
+                                <div className={styles.t3}>
+                                    {participant?.email}
+                                </div>
+                                <div className={styles.t4}>
+                                    <span
+                                        class="form-item-icon material-symbols-rounded"
+                                        className={styles.icon}
+                                    >
+                                        <DeleteOutlineIcon />
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
-    )
+    );
 }
 
-export {MeetingParticipantsTable};
+export { MeetingParticipantsTable };
