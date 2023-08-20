c6f72facc90f8ced1b8557e073789a5796f2a741
Gavin Qian <gqia0002@student.monash.edu>
2023-08-20 20:39:08 +1000

    [Done] Added styling for the bordered headers on 'upload' and 'meeting details'

--- sem2/frontend/src/components/main/UploadScreen.jsx
+++ sem2/frontend/src/components/main/UploadScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { HeaderPill } from "../HeaderPill";
 import styles from "./Dashboard.module.css";
 import { MeetingParticipantsTable } from "../meeting/MeetingParticipantsTable";
+import { BorderedHeading } from "../BorderedHeading";
 
 function UploadScreen() {
     const [activeScreen, setActiveScreen] = useState("RecordingUpload");
@@ -14,17 +15,21 @@ function UploadScreen() {
     };
 
     const addParticipant = (name, email) => {
-        const newParticipant = { 
+        const newParticipant = {
             id: participants.length + 1,
             name: name,
-            email: email
+            email: email,
         };
-        setParticipants(prev => [...prev, newParticipant]);
+        setParticipants((prev) => [...prev, newParticipant]);
     };
 
     const screenComponents = {
-        RecordingUpload: <RecordingUploadScreen onAddParticipant={addParticipant} />,
-        MeetingParticipants: <MeetingParticipantsScreen participants={participants} />,
+        RecordingUpload: (
+            <RecordingUploadScreen onAddParticipant={addParticipant} />
+        ),
+        MeetingParticipants: (
+            <MeetingParticipantsScreen participants={participants} />
+        ),
     };
 
     return (
@@ -54,8 +59,7 @@ function UploadScreen() {
     );
 }
 
-function RecordingUploadScreen({onAddParticipant}) {
-
+function RecordingUploadScreen({ onAddParticipant }) {
     const [showAddParticipants, setShowAddParticipants] = useState(false);
     const [participantName, setParticipantName] = useState("");
     const [participantEmail, setParticipantEmail] = useState("");
@@ -110,9 +114,7 @@ function RecordingUploadScreen({onAddParticipant}) {
     return (
         <>
             <div className={styles.upload_page}>
-                <div className={styles.upload_heading}>
-                    Upload new recording
-                </div>
+                <BorderedHeading name="Upload new recording" />
                 <div className={styles.file_upload_box}>
                     <div className={styles.file_upload_header_1}>
                         Upload Recording
@@ -201,23 +203,30 @@ function RecordingUploadScreen({onAddParticipant}) {
                     project meetings? To help 'debug' the complexities!",
                 </div>
                 {showAddParticipants && (
-                <div>
-                    <input 
-                        type="text" 
-                        placeholder="Name" 
-                        value={participantName} 
-                        onChange={e => setParticipantName(e.target.value)} 
-                    />
-                    <input 
-                        type="email" 
-                        placeholder="Email" 
-                        value={participantEmail} 
-                        onChange={e => setParticipantEmail(e.target.value)} 
-                    />
-                    <button onClick={handleSubmitParticipant}>Submit</button>
-                </div>
-            )}
-                <button className={styles.add_participants_button} onClick={handleAddParticipantsClick}>
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            value={participantName}
+                            onChange={(e) => setParticipantName(e.target.value)}
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={participantEmail}
+                            onChange={(e) =>
+                                setParticipantEmail(e.target.value)
+                            }
+                        />
+                        <button onClick={handleSubmitParticipant}>
+                            Submit
+                        </button>
+                    </div>
+                )}
+                <button
+                    className={styles.add_participants_button}
+                    onClick={handleAddParticipantsClick}
+                >
                     Add Meeting Participants
                 </button>
             </div>
@@ -225,39 +234,10 @@ function RecordingUploadScreen({onAddParticipant}) {
     );
 }
 
-function MeetingParticipantsScreen({participants}) {
-
+function MeetingParticipantsScreen({ participants }) {
     return (
         <>
-            <div className={styles.summary_heading}>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="656"
-                    height="16"
-                    viewBox="0 0 656 16"
-                    className={styles.summary_heading_line}
-                >
-                    <path
-                        d="M639.993 8.03713C640.014 12.4554 643.612 16.0204 648.03 15.9999C652.448 15.9795 656.013 12.3812 655.993 7.96294C655.972 3.54471 652.374 -0.0203682 647.956 0.000118163C643.538 0.0206045 639.973 3.6189 639.993 8.03713ZM1.00696 12.5L648 9.50002L647.986 6.50005L0.993045 9.50002L1.00696 12.5Z"
-                        fill="#E9F9FF"
-                        fill-opacity="0.25"
-                    />
-                </svg>
-                Edit Meeting Participants
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="651"
-                    height="16"
-                    viewBox="0 0 651 16"
-                    className={styles.summary_heading_line}
-                >
-                    <path
-                        d="M3.86238e-05 7.97508C-0.0137254 12.3933 3.55682 15.9862 7.97508 16C12.3933 16.0137 15.9862 12.4432 16 8.02492C16.0137 3.60667 12.4432 0.0138027 8.02492 3.86238e-05C3.60667 -0.0137254 0.0138027 3.55682 3.86238e-05 7.97508ZM7.99533 9.49999L649.995 11.5L650.005 8.50001L8.00467 6.50001L7.99533 9.49999Z"
-                        fill="#E9F9FF"
-                        fill-opacity="0.25"
-                    />
-                </svg>
-            </div>
+            <BorderedHeading name="Edit Meeting Participants" />
             <MeetingParticipantsTable participants={participants} />
         </>
     );
