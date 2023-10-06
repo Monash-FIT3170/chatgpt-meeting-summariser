958ac3ea55a7bc76dca277ce89adec95ad8c264f
ttan0053 <ttan0053@student.monash.edu>
2023-10-06 15:14:25 +1100

    [DONE] Added route /summaryDotPoints for summary dot points

--- project/backend/routes/summary.js
+++ project/backend/routes/summary.js
@@ -94,6 +94,79 @@ router.route("/summary").post(async (req, res) => {
         });
 })
 
+router.route("/summaryDotPoints").post(async (req, res) => {
+    const transcriptChunks = transcribedScript
+    let summaryChunks = ""
+    let transcript = ""
+    console.log("no. chunks:", transcriptChunks.length)
+    for (var i = 0; i < transcriptChunks.length; i++) {
+        console.log("summarising chunk", i+1, "of", transcriptChunks.length)
+        let transcriptChunk = transcriptChunks[i].text
+        transcript = transcript.concat(transcriptChunk)
+
+        const chunkCompletion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are a meeting assistant that is tasked to summarize meeting transcripts.",
+                },
+                {
+                    role: "user",
+                    content:
+                        `Please generate a meeting summary for the following transcript .`,
+                },
+                {
+                    role: "assistant",
+                    content: "Sure, I will generate a summary for your meeting transcript.",
+                },
+                {role: "user", content: transcriptChunk},
+            ],
+        });
+
+        let summaryChunk = chunkCompletion.data.choices[0].message.content
+        summaryChunks = summaryChunks.concat("\n", summaryChunk)
+    }
+
+    let fullCompletion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "system",
+                content:
+                    "You are a meeting assistant that is tasked to summarize meeting transcripts in dot points.",
+            },
+            {
+                role: "user",
+                content:
+                    "In the next message I will supply summaries of several parts of a meeting. Please combine the following meeting summaries into a single cohesive meeting summary.",
+            },
+            {
+                role: "assistant",
+                content: "Absolutely, I'm ready to assist. Please provide me with the meeting summaries that you'd like me to combine into a cohesive summary, and I'll do my best to create a comprehensive summary for you.",
+            },
+            {role: "user", content: summaryChunks},
+        ],
+    });
+
+    const summaryPoints = fullCompletion.data.choices[0].message.content;
+
+    const newMeetingSummary = new MeetingSummary({transcript: transcript, summaryPoints});
+    await newMeetingSummary
+        .save()
+        .then((savedMeetingSummary) => {
+            const savedMeetingSummaryId = savedMeetingSummary._id;
+            console.log(savedMeetingSummaryId)
+            res.json({ id: savedMeetingSummaryId });
+
+          })
+        .catch((err) => {
+            console.log("save summary failed")
+            res.status(400).json("Error: " + err)
+        });
+})
+
 
 
 router.delete('/api/delete/:id', async (req, res) => {
